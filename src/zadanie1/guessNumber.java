ad06f50e2521507d2253f9f72bf4a9375141fcd5
Przemyslaw Pycko <pyckop@mailbox.org>
2018-12-16 14:10:28 +0100

    Zadanie 1: Gra w zgadywanie liczb

---
+++ src/zadanie1/guessNumber.java
@@ -0,0 +1,50 @@
+package zadanie1;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessNumber {
+
+    public static void main(String[] args) {
+
+        guessNumber();
+
+    }
+
+    static int getInt() {
+        int input = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Zgadnij liczbę: ");
+        while (scan.hasNext()) {
+            try {
+                input = scan.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                scan.next();
+                System.out.println("To nie jest liczba.");
+                System.out.print("Zgadnij liczbę: ");
+            }
+        }
+        return input;
+    }
+
+
+    static void guessNumber () {
+        Random r = new Random();
+        int toGuess = r.nextInt(100) + 1;
+        int input = 0;
+
+        while (input != toGuess) {
+            input = getInt();
+            if (input < toGuess) {
+                System.out.println("Za mało!");
+            } else if (input > toGuess){
+                System.out.println("Za dużo!");
+            }
+
+        }
+        System.out.println("Zgadłeś!");
+
+    }
+}
